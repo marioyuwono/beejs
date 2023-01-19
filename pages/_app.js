023e4903f00acb04325ffc23205c7bb1475b9c47
marioyuwono <59589638+marioyuwono@users.noreply.github.com>
2023-01-19 14:16:21 +0700

    Install Service Worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,37 @@
+import { useEffect } from 'react'
 import UserProvider from '@context/user'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+async function registerSW() {
+	// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+	try {
+		const registration = await navigator.serviceWorker.register("/sw.js", {
+			scope: "/",
+		})
+		if (registration.installing) {
+			console.log("Service worker installing")
+		} else if (registration.waiting) {
+			console.log("Service worker installed")
+		} else if (registration.active) {
+			console.log("Service worker active")
+		}
+	} catch (error) {
+		console.error(`Registration failed with ${error}`)
+	}
+}
+
 export default function App({ Component, pageProps }) {
-    return (
-        <UserProvider>
-            <Component {...pageProps}/>
-        </UserProvider>
-    )
+	useEffect(() => {
+		if (navigator.serviceWorker) {
+			console.log('Registering Service Worker')
+			registerSW()
+		} else {
+			console.warn('Service Worker is not available')
+		}
+	}, [])
+	return (
+		<UserProvider>
+			<Component {...pageProps} />
+		</UserProvider>
+	)
 }
---
+++ public/sw.js
@@ -0,0 +1,3 @@
+self.addEventListener("install", function (e) {
+	console.log("Hello world from the Service Worker")
+})
